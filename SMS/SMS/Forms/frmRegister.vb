eb93fa0fb3919c13548871f1230aa26f22f27be9
marzxxx90 <junmar_honor@ymail.com>
2017-04-25 15:26:53 +0800

    Update

--- SMS/SMS/Forms/frmRegister.vb
+++ SMS/SMS/Forms/frmRegister.vb
@@ -2,12 +2,12 @@
 Imports System.Text
 
 Public Class frmRegister
-    Private SMSHash As New Hashtable
     Private pattern As String = "\[.*?\]"
     Private strParse As String, strTrim As String
     Private mc As System.Text.RegularExpressions.MatchCollection
 
     Private Function Parser(ByVal StrText As String) As Hashtable
+        Dim SmsHash As New Hashtable
         Dim regx As New System.Text.RegularExpressions.Regex(pattern)
         If Regex.IsMatch(StrText, pattern) Then
             mc = regx.Matches(StrText) 'Check kong pila ang bracket
@@ -22,7 +22,7 @@ Public Class frmRegister
 
                 If Not isValidKey(strParse) Then Exit For
 
-                SMSHash.Add(strParse, strTrim)
+                SmsHash.Add(strParse, strTrim)
             Next
         End If
 
@@ -46,6 +46,30 @@ Public Class frmRegister
         Return SMSHash
     End Function
 
+    Private Function Parser2(ByVal smg As String) As Hashtable
+        Dim SmsHash As New Hashtable
+
+        Dim regx As New System.Text.RegularExpressions.Regex(pattern)
+        If Regex.IsMatch(smg, pattern) Then
+            mc = regx.Matches(smg) 'Check kong pila ang bracket
+            For index As Integer = 0 To mc.Count - 1 ' Count sa Bracket
+                'Nagkuha ug Text Fields
+                strParse = mc(index).Value.Substring(0, mc(index).Value.IndexOf(":")).Trim({"["c})
+                Console.WriteLine("Fields " & strParse)
+
+                'Nagkuha ug Fields Information
+                strTrim = mc(index).Value.Substring(mc(index).Value.IndexOf(":") + 1).Trim({"]"c}).Trim({" "c})
+                Console.WriteLine("Value " & strTrim)
+
+                If Not isValidKey(strParse) Then Exit For
+
+                SmsHash.Add(strParse, strTrim)
+            Next
+        End If
+
+
+        Return SmsHash
+    End Function
 
     Private Function isValidKey(ByVal Keys As String) As Boolean
         Dim KeysCollection() As String = {"FNAME", "MNAME", "LNAME", "NUMBER", "REG_KEY"}
@@ -56,9 +80,22 @@ Public Class frmRegister
     End Function
 
     Private Sub btnOk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOk.Click
-        Parser(txtParse.Text)
-        For Each DE As DictionaryEntry In SMSHash
-            Console.WriteLine("Key " & DE.Key & " Value " & DE.Value)
+        'Parser(txtParse.Text)
+        'For Each DE As DictionaryEntry In SMSHash
+        '    Console.WriteLine("Key " & DE.Key & " Value " & DE.Value)
+        'Next
+
+
+        For Each de As DictionaryEntry In Parser2(txtParse.Text)
+            Console.WriteLine("Key " & de.Key & " Value " & de.Value)
+            Dim strSplit As String()
+            Dim str As String = de.Value
+            strSplit = str.Split("/")
+
+            Console.WriteLine(".......................")
+            For Each s As String In strSplit
+                Console.WriteLine("Values " & s)
+            Next
         Next
     End Sub
 End Class
